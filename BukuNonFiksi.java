912a5a8ece54ee7ab0b7a3b51502fc857e42297a
Leovander Aditama <leovanderadds@gmail.com>
2024-03-08 21:32:23 +0700

    added turunan buku nonfiksi

---
+++ BukuNonFiksi.java
@@ -0,0 +1,16 @@
+// Kelas BukuNonFiksi (turunan dari Buku)
+public class BukuNonFiksi extends Buku {
+  private String subjek;
+
+  public BukuNonFiksi(String judul, String penulis, int tahunTerbit, String subjek) {
+    super(judul, penulis, tahunTerbit);
+    this.subjek = subjek;
+  }
+
+  // Method override tampilkanInfo
+  @Override
+  public void tampilkanInfo() {
+    super.tampilkanInfo();
+    System.out.println("Subjek: " + subjek);
+  }
+}
